671d28e71702a118d1efc39edc2abeb49c85ddba
LockScrollOn <riceblackflame@gmail.com>
2025-12-29 19:47:03 -0600

    Eliminar carpeta dublicada por error

--- TEC-SHOP/backend/src/controllers/healt-controller/healt-controller.js
+++
@@ -1,6 +0,0 @@
-export const healtCheck  = (req, res)  => {
-    return res.status(200).json({
-        status: "ok",
-        message: "Back-end funcionando"
-    })
-}
--- TEC-SHOP/backend/src/routes/healt-route/healt-route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { healtCheck } from "../../controllers/healt-controller/healt-controller.js";
-
-const router = Router();
-
-router.get("/healt", healtCheck);
-
-export default router;
