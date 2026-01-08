6916ce5eff8aff96e5149d956d5b2d6d46a0ffc3
romancampos-prog <camprogramroman@gmail.com>
2026-01-08 07:33:37 -0600

    fix(frontend): ajuste de rutas y overlay de conexión

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,16 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Incio_page from "../pages/inicio/inicio-page";
 import Login from "../pages/login/Login";
 import AgregarProducto from "../pages/subirProducto/AgregarProducto";
-import SinConexion from "../components/sinConexion/sinConexion";
-import useConexion from "../hooks/usarConexion";
+
 
 export default function AppRoutes() {
-  const enLinea = useConexion();
+  
 
   return (
     <BrowserRouter>
-      <SinConexion visible={!enLinea} />
-
       <Routes>
         <Route path="/subirProducto" element={<AgregarProducto />} />
         <Route path="/inicio" element={<Incio_page />} />
